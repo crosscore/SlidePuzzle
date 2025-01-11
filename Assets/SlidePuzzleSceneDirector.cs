ff1739b61d02033ffa0f081fd3ada36298e4f566
yuu <crosscore.g@gmail.com>
2025-01-12 03:15:37 +0900

    Add: ShufflePieces()

--- Assets/SlidePuzzleSceneDirector.cs
+++ Assets/SlidePuzzleSceneDirector.cs
@@ -5,29 +5,27 @@ public class SlidePuzzleSceneDirector : MonoBehaviour
 {
     // ピースのリスト
     [SerializeField] List<GameObject> pieces;
+
     // クリア時に表示されるリトライボタン
     [SerializeField] GameObject buttonRetry;
+
     // ゲーム開始時にピースをシャッフルする回数
     [SerializeField] int shuffleCount;
+
     // ゲーム開始時のピースの初期位置のリスト
     List<Vector2> startPositions;
+
     void Start()
     {
-        // 0番のピースと隣接するピースのリスト
-        List<GameObject> movablePieces = new List<GameObject>();
-
-        // 0番のピースと隣接するピースをリストに追加
-        foreach (var piece in pieces) {
-            if (GetEmptyPiece(piece) != null) {
-                movablePieces.Add(piece);
-            }
+        // ピースの初期位置を保存
+        startPositions = new List<Vector2>();
+        foreach (var piece in pieces)
+        {
+            startPositions.Add(piece.transform.position);
         }
-        print($"movablePieces.Count: {movablePieces.Count}");
 
-        // 隣接するピースをランダムに選択して入れ替える
-        int rnd = Random.Range(0, movablePieces.Count);
-        GameObject selectedPiece = movablePieces[rnd];
-        SwapPiece(selectedPiece, pieces[0]);
+        // ゲーム開始時にピースをシャッフル
+        ShufflePieces();
 
         // ゲーム開始時はリトライボタンを非表示
         buttonRetry.SetActive(false);
@@ -37,17 +35,41 @@ public class SlidePuzzleSceneDirector : MonoBehaviour
     void Update()
     {
         // マウス入力があった場合
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0))
+        {
             // クリックした位置にあるピースを取得
             Vector2 tapPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             // raycast でクリックした位置にあるオブジェクトを取得
             RaycastHit2D hit = Physics2D.Raycast(tapPoint, Vector2.zero);
             // 衝突判定があった場合
-            if (hit) {
+            if (hit)
+            {
                 // クリックしたピースを取得
                 GameObject histPiece = hit.collider.gameObject;
                 // クリックしたピースと0番のピースを入れ替える
                 SwapPiece(histPiece, GetEmptyPiece(histPiece));
+
+                // クリア判定
+                buttonRetry.SetActive(true);
+                // クリア時に全てのピースが初期位置にあるか判定
+                for (int i = 0; i < pieces.Count; i++)
+                {
+                    // 現在のポジション
+                    Vector2 currentPos = pieces[i].transform.position;
+                    // 初期位置
+                    Vector2 startPos = startPositions[i];
+                    // 位置が一致していない場合はクリアしていない
+                    if (currentPos != startPos)
+                    {
+                        buttonRetry.SetActive(false);
+                        break;
+                    }
+                }
+                // クリア状態の場合はリトライボタンを表示
+                if (buttonRetry.activeSelf)
+                {
+                    Debug.Log("Clear!");
+                }
             }
         }
     }
@@ -56,7 +78,8 @@ public class SlidePuzzleSceneDirector : MonoBehaviour
     GameObject GetEmptyPiece(GameObject piece)
     {
         float dist = Vector2.Distance(piece.transform.position, pieces[0].transform.position);
-        if (dist == 1) {
+        if (dist == 1)
+        {
             return pieces[0];
         }
         return null;
@@ -65,11 +88,46 @@ public class SlidePuzzleSceneDirector : MonoBehaviour
     // 2つのピースの位置を入れ替える
     void SwapPiece(GameObject piece1, GameObject piece2)
     {
-        if (piece1 == null || piece2 == null) {
+        if (piece1 == null || piece2 == null)
+        {
             return;
         }
         Vector2 temp = piece1.transform.position;
         piece1.transform.position = piece2.transform.position;
         piece2.transform.position = temp;
+    }
+
+    // リトライボタンをクリックした時の処理
+    public void OnClickRetryButton()
+    {
+        print("OnClickRetryButton");
+        // ピースをシャッフル
+        ShufflePieces();
+
+        // リトライボタンを非表示
+        buttonRetry.SetActive(false);
+    }
+
+    // ピースをシャッフルする関数
+    private void ShufflePieces()
+    {
+        for (int i = 0; i < shuffleCount; i++)
+        {
+            // 0番のピースと隣接するピースのリスト
+            List<GameObject> movablePieces = new List<GameObject>();
+
+            // 0番のピースと隣接するピースをリストに追加
+            foreach (var piece in pieces)
+            {
+                if (GetEmptyPiece(piece) != null)
+                {
+                    movablePieces.Add(piece);
+                }
+            }
+            // 隣接するピースをランダムに選択して入れ替える
+            int rnd = Random.Range(0, movablePieces.Count);
+            GameObject selectedPiece = movablePieces[rnd];
+            SwapPiece(selectedPiece, pieces[0]);
+        }
     }
 }
